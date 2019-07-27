54cbe6c880eaeb7c5a70a746de8e263021b0fb57
Prasun <prasunmukul@gmail.com>
2019-07-27 14:22:43 +0530

    commit to update project

---
+++ TicketBookingManagementAppMysql/src/main/java/com/springbootLearn/bookTicket/app/TicketBookingManagementAppMysql/TicketBookingManagementAppMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.springbootLearn.bookTicket.app.TicketBookingManagementAppMysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingManagementAppMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingManagementAppMysqlApplication.class, args);
+	}
+
+}
---
+++ TicketBookingManagementAppMysql/src/test/java/com/springbootLearn/bookTicket/app/TicketBookingManagementAppMysql/TicketBookingManagementAppMysqlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springbootLearn.bookTicket.app.TicketBookingManagementAppMysql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TicketBookingManagementAppMysqlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
